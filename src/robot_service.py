346b11f5be1843f875bf7c5745b5c904009ee4d4
Mingyu Wang <x0w0x@Mingyus-MacBook-Pro.local>
2021-05-05 08:21:47 -0700

    add code in detectChessboard,need ros service to communicate with the motion_planning

--- src/robot_service.py
+++ src/robot_service.py
@@ -1,3 +1,5 @@
+import numpy as np 
+import numpy.linalg as LA
 from chessboard_state_detection import ChessboardStateDetection
 #from robotic_chess_player.srv import PlayerService, PlayerServiceReponse
 
@@ -11,6 +13,7 @@ class RobotService:
         ## initialize camera topic subscriber
 
         ## setup PlayerService
+        self.action_srv = rospy.ServiceProxy('action_service', Action)#send command to moveit and execute
 
         pass
 
@@ -28,16 +31,31 @@ class RobotService:
 
         if req.request == "detect chessboard state":
             
-
+    @staticmethod       
+    def imageFromTopic():
+        img_msg = rospy.wait_for_message('avt_camera_img', Image)
+        bridge = CvBridge()
+        return bridge.imgmsg_to_cv2(msg, "bgr8")
     
     def detectChessbaord(self):
-        ## move robot to standby position
-
+        ## move robot to general standby position
+        general_standby = []
+        manipulator = self.action_srv
+        pose_estimator = self.chessboardPoseEstimation
+        #send to moveit the wait for it done
+        manipulator('joint,[]')
         ## take one image
+        image = self.imageFromTopic()
+        ## do pose estimation
+        rot_vec,trans_vec = pose_estimator.pose_estimation(image)
+        ## adjust the standby position
+        ori_vec = trans_vec/LA.norm(trans_vec)
+        dot_product = np.dot(ori_vec, zero_direction)
+        general_standby[0] = np.arccos(dot_product)
+        ## send to moveit 
+        manipulator('joint,[]')
+        ## save the information
 
-        ## do pose estimation for the chessboard
-
-        ## save information.
         self.chessboardStateDetection.setSquarePos(square_pos)
     
     def moveChess(self, request):
